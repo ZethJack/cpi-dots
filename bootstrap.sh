aac6767091bf8c77d4f6e5d8f4cd0319155822e4
Zeth <zeth@zethjack.eu>
2025-01-14 17:33:10 +0100

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,26 @@
 # exit on error
 set -e
 # Download and install nix for all users
-echo "Downloading and installing nix..."
-sh <(curl -L https://nixos.org/nix/install) --daemon
+case "$(command -v nix-env)" in
+    "")
+        echo "Downloading and installing nix..."
+        sh <(curl -L https://nixos.org/nix/install) --daemon
+        ;;
+    *)
+        echo "Nix is already installed. Skipping installation..."
+        ;;
+esac
 
 #activate experimental features
 echo "Activating flakes..."
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+
+# Install Home Manager
+echo "Installing Home Manager..."
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
 
 # install flatpak
 echo "Installing flatpak..."
