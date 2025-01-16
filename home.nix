ee82792bcc4d0563deec82a23fab4851ab546e4f
Zeth <zeth@zethjack.eu>
2025-01-16 16:51:16 +0100

    lsp install?

--- home.nix
+++ home.nix
@@ -34,6 +34,8 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
     tofi
+    nixd
+    alejandra
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
