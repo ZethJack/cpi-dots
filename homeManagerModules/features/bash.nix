43ae9265a8115965bb7ec186dc03caaa328be234
Zeth <zeth@zethjack.eu>
2025-02-15 03:57:19 +0100

    bash settings

--- homeManagerModules/features/bash.nix
+++ homeManagerModules/features/bash.nix
@@ -1,5 +1,36 @@
 {...}: {
   programs.bash = {
     enable = true;
+    enableCompletion = true;
+    historyControl = "ignoreboth";
+    historySize = 1000;
+    historyFileSize = 2000;
+    shellOptions = [
+      "histappend"
+      "checkwinsize"
+    ];
+    shellAliases = {
+      "ccd" = "cd ~/.local/src/dotfiles";
+      "yt" = "yt-dlp --add-metadata -i --external-downloader aria2c:\"-c -j 3 -x 3 -k 1M\" --sponsorblock-remove sponsor,selfpromo,interaction -o \"%(title)s.\(ext)s\"";
+      "yta" = "yt -x -f bestaudio/best --audio-format opus";
+      "yta-ogg" = "yt -x -f bestaudio/best --audio-format ogg";
+      "tat" = "tmux a || tmux";
+      "nhhs" = "nh home switch";
+      "mkd" = "mkdir -pv";
+      "cdd" = "cd ~/.local/src/dotfiles";
+      "cdf" = "cdd ; hx \$(pwd)";
+      "cdb" = "cdd ; nhhs";
+      "lg" = "lazygit";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  programs.oh-my-posh = {
+    enable = true;
+    enableBashIntegration = true;
+    useTheme = "catppuccin_mocha";
   };
 }
