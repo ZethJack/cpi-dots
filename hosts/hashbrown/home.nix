f843818e73816f95381afb48d7adf8679ad3a442
Zeth <zeth@zethjack.eu>
2025-02-18 15:59:49 +0100

    hashbrown added

---
+++ hosts/hashbrown/home.nix
@@ -0,0 +1,121 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  home.username = "zeth";
+  home.homeDirectory = "/home/zeth";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+  myHomeManager = {
+    bundles.general.enable = true;
+  };
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+    # ".bashrc".source = ../../files/.bashrc;
+    # ".bash_aliases".source = ../../files/.bash_aliases;
+    ".local/bin" = {
+      source = ../../scripts;
+      recursive = true;
+    };
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/zeth/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    PASSWORD_STORE_DIR = "$HOME/.local/share/password_store";
+    PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs = {
+    mpv = {
+      enable = true;
+      config = {
+        vo = "gpu";
+        hwdec = "mmal";
+        gpu-context = "wayland";
+        ao = "pipewire";
+        cache-secs = 10;
+        cache-pause = "yes";
+      };
+    };
+    git = {
+      enable = true;
+      userName = "Zeth";
+      userEmail = "zeth@zethjack.eu";
+    };
+    gh = {
+      enable = true;
+    };
+    password-store = {
+      enable = true;
+      package = pkgs.pass.withExtensions (exts: [exts.pass-otp]);
+      settings = {
+        PASSWORD_STORE_DIR = "$HOME/.local/share/password_store";
+        PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
+      };
+    };
+    nh = {
+      enable = true;
+      flake = "/home/zeth/.local/src/dotfiles";
+    };
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+    pinentryPackage = pkgs.pinentry-gtk2;
+  };
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = ["JetBrainsMono Nerd Font Mono"];
+      sansSerif = ["JetBrainsMono Nerd Font"];
+      serif = ["JetBrainsMono Nerd Font"];
+    };
+  };
+}
